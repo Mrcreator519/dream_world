cd06d68a86b447f892a5fc0f6a4ee1efc5d9ee43
Mrcreator519 <mohits.23149@obhs.school.nz>
2025-07-31 09:11:36 +1200

    Create dreamworld.py
    intro to dreamworld

---
+++ dreamworld.py
@@ -0,0 +1,14 @@
+'''
+    author: Mohit Singh
+    date: 31/07/2025
+    version: 1
+    description: intro to dreamworld
+'''
+#------libary-----------------
+import random
+#------functions--------------
+
+
+
+#-----main routine------------
+print('Hello World')
